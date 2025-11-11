fd176fc01fe72104f28e2cfbda34c2e570368544
anaailiesei <ana@DESKTOP-0D79E4D>
2025-11-11 19:05:35 +0200

    b2 e gata

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/Constants.kt
@@ -0,0 +1,7 @@
+package ro.pub.cs.systems.eim.practicaltest01var06
+
+interface Constants {
+    companion object {
+        const val TAG = "APPLICATION"
+    }
+}
--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06SecondActivity.kt
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06SecondActivity.kt
@@ -1,4 +1,82 @@
 package ro.pub.cs.systems.eim.practicaltest01var06
 
-class PracticalTest01Var06SecondActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class PracticalTest01Var06SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_layout)
+
+        val val1 = intent.getStringExtra("key1")
+        val val2 = intent.getStringExtra("key2")
+        val val3 = intent.getStringExtra("key3")
+
+        val num = intent.getIntExtra("key4", 0)
+        var gained = false
+
+        if (val1 == "*") {
+            if (val2 == "*") {
+                gained = true
+            } else {
+                if (val3 == "*") {
+                    gained = true
+                } else {
+                    gained = val2 == val3
+                }
+            }
+        } else {
+            if (val2 == "*") {
+                if (val3 == "*") {
+                    gained = true
+                } else {
+                    gained = val1 == val3
+                }
+            } else {
+                if (val3 == "*") {
+                    gained = val2 == val1
+                } else {
+                    gained = (val1 == val2) && (val1 == val3)
+                }
+            }
+        }
+
+
+
+
+        var scor : Int
+
+        if (!gained) {
+            scor = 0
+        } else if (num == 0) {
+            scor =100
+        } else if (num ==1) {
+            scor = 50
+        } else {
+            scor  = 10
+        }
+
+        var data : String
+        if (gained) {
+            data = "Gained $scor"
+        } else {
+            data = "0"
+        }
+        (findViewById(R.id.gained_text_view) as TextView).setText(data)
+
+        // Presupunând că vrei să returnezi rezultatul la un anumit eveniment, de exemplu, la apăsarea unui buton
+        val someButton: Button = findViewById(R.id.ok)
+//
+        someButton.setOnClickListener {
+            val returnIntent = Intent().apply {
+                putExtra("another_key", scor)
+            }
+            setResult(RESULT_OK, returnIntent)
+            finish()
+        }
+        // Dacă activitatea se încheie fără a seta explicit un rezultat, poți să nu faci nimic sau să setezi RESULT_CANCELED
+    }
 }
